76d688399395ad7324d48a84159b7506926e7611
KostaLekos <github.shiny670@passmail.com>
2026-02-06 15:03:57 -0800

    Began on backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const { exec } = require("child_process");
+
+const JAVA_DIR = "/full/path/to/your/nginx/html";
+const JAVA_CMD = "java Choice";
+
+http.createServer((req, res) => {
+  if (req.method === "POST" && req.url.startsWith("/run")) {
+
+    const param = "testparam";
+
+    exec(`${JAVA_CMD} ${param}`, { cwd: JAVA_DIR }, (err, stdout, stderr) => {
+      if (err) {
+        res.writeHead(500);
+        res.end(stderr);
+        return;
+      }
+      res.end(stdout);
+    });
+
+  } else {
+    res.writeHead(404);
+    res.end("no");
+  }
+}).listen(30201, "127.0.0.1");
+
+console.log("Node server running on 127.0.0.1:30201");
---
+++ html/Choice.java
@@ -0,0 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+public class Choice {
+    public static void main(String[] args) throws IOException {
+
+        // I know that this has no reason to be written in binary but I need it to be in binary for my assignment
+        Binary bin = new Binary();
+        Path file = Path.of("choice.txt");
+
+        String b = bin.toBinary(args[0]);
+
+        Files.writeString(file, b);
+    }
+}
